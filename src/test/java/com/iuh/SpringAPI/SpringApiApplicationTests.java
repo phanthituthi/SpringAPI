e101a4dd0806b3fabd11f130de203a879edbaf83
phanthituthi <83085963+phanthituthi@users.noreply.github.com>
2021-09-15 18:45:57 +0700

    Initial commit

---
+++ src/test/java/com/iuh/SpringAPI/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.SpringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
